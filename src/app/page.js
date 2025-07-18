f5a69f53f8705d6d41a30fa5c51ebddcab98a518
tandat <trantandat0723@gmail.com>
2025-07-19 03:44:23 +0700

    feat(UI): add UI navbar and other ingredients

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar/navbar";
 
 export default function Home() {
   return (
-    <main className='flex flex-col min-h-screen bg-[#121212] container mx-auto px-12 py-4'>
-      <HeroSection />
+    <main className='flex min-h-screen flex-col bg-[#121212] '>
+      <Navbar />
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
